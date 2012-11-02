b671b2444a8a0b355e6ccf84240af3d706ac49b6
cameljava <cameljava@gmail.com>
2012-11-02 16:18:28 +1100

    first commit. after change to maven project

---
+++ Hello/src/main/java/org/ktest/Hello/App.java
@@ -0,0 +1,13 @@
+package org.ktest.Hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

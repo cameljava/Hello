3e1e0de3d1f733831738d7e0a5e41e6c2ef75785
cameljava <cameljava@gmail.com>
2012-11-09 03:04:39 +0000

    commit two changes

--- Hello/src/main/java/org/ktest/Hello/App.java
+++ Hello/src/main/java/org/ktest/Hello/App.java
@@ -1,5 +1,14 @@
 package org.ktest.Hello;
 
+import javax.xml.XMLConstants;
+
+import org.omg.CORBA.portable.ApplicationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
 /**
  * Hello world!
  *
@@ -8,6 +17,13 @@ public class App
 {
     public static void main( String[] args )
     {
+    	Logger logger=LoggerFactory.getLogger(App.class);
         System.out.println( "Hello World!" );
+        logger.info("Hello by logback.");
+        
+        ApplicationContext ap = new ClassPathXmlApplicationContext("spring/beans.xml");
+        TestProperties testProperties = ap.getBean(TestProperties.class);
+        System.out.println("testsetst"+testProperties);
+        logger.info(testProperties.getProperty1());
     }
 }

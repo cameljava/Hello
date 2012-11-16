ad0c54fa973458e31cabec4d482ee90d9aadcd87
cameljava <cameljava@gmail.com>
2012-11-16 12:00:02 +1100

    add some chage

---
+++ Hello/src/main/java/org/ktest/Hello/TestProperties.java
@@ -0,0 +1,20 @@
+package org.ktest.Hello;
+
+public class TestProperties {
+	private String str1;
+	
+//	private String property1;
+
+	public String getProperty1() {
+		return str1;
+//		return property1;
+	}
+
+	public void setProperty1(String str1) {
+		this.str1 = str1;
+//		this.property1=str1;
+	}
+	
+	
+
+}
